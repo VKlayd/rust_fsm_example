7137c0f64084d6c3738554c2db30e9ab1f3e1b3b
Victor Korkin <victor.korkin@gmail.com>
2017-09-24 22:04:52 +0700

    Add machines interaction.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-#[macro_use]
 extern crate chan;
-extern crate threadpool;
 
 #[macro_use] mod macro_machine;
 
-use threadpool::ThreadPool;
 use std::thread;
-use std::sync::{Arc, Mutex};
+
+pub enum Message {
+    Test {id: usize, cmd: Test::Commands},
+    OtherTest {id: usize, cmd: OtherTest::Commands}
+}
 
 declare_machine!(
-    Test context{id: i16}(State1)
+    Test context{id: i16, tx: chan::Sender<Message>}(State1)
     states[State1, State2, State3]
     commands[Cmd1, Cmd2]
 
     (State1 cont:
-        Cmd1 {println!("{} {:?} exec Cmd1", context.id, cont);}=> State2;
+        Cmd1 {
+            println!("{} {:?} exec Cmd1", context.id, cont);
+            if (context.id == 0) {
+                context.tx.send(Message::OtherTest{id:0, cmd: OtherTest::Commands::Cmd1});
+            }}=> State2;
     )
     (State2 cont:
-        Cmd1 {println!("{} {:?} exec Cmd1", context.id, cont);thread::sleep(std::time::Duration::from_secs(1));println!("wait {} done", context.id);}=> State1;
+        Cmd1 {println!("{} {:?} exec Cmd1", context.id, cont);}=> State1;
         Cmd2 {println!("{} {:?} exec Cmd2", context.id, cont);}=> State3;
     )
     (State3 cont:
@@ -25,32 +30,77 @@ declare_machine!(
     )
 );
 
-enum Message {
-    Test {id: usize, cmd: Test::Commands}
-}
+declare_machine!(
+    OtherTest context{id: i16, tx: chan::Sender<Message>}(State1)
+    states[State1, State2, State3]
+    commands[Cmd1, Cmd2]
+
+    (State1 cont:
+        Cmd1 {println!("> {} {:?} exec Cmd1", context.id, cont);}=> State2;
+    )
+    (State2 cont:
+        Cmd1 {println!("> {} {:?} exec Cmd1", context.id, cont);}=> State1;
+        Cmd2 {println!("> {} {:?} exec Cmd2", context.id, cont);}=> State3;
+    )
+    (State3 cont:
+        Cmd1 {println!("> {} {:?} exec Cmd1", context.id, cont);}=> State1;
+    )
+);
 
 fn main() {
-    let mut test_machines = vec!(Test::new(0), Test::new(1), Test::new(2));
-    let n_workers = 2;
-    let pool = ThreadPool::new(n_workers);
+    let (tx, rx) = chan::sync::<Message>(10);
 
-    let (tx, rx) = chan::sync::<Message>(6);
+    let test_machines = vec!(Test::new(0, tx.clone()), Test::new(1, tx.clone()), Test::new(2, tx.clone()));
+    let othertest_machines = vec!(OtherTest::new(0, tx.clone()));
 
+    let (tx_fin, rx_fin) = chan::sync(0);
     let mut channels: Vec< chan::Sender<Test::Commands> > = std::vec::Vec::new();
+    let mut other_channels: Vec< chan::Sender<OtherTest::Commands> > = std::vec::Vec::new();
     for ma in test_machines {
-        let (tx, rx) = chan::sync::<Test::Commands>(6);
+        let (tx, rx) = chan::sync::<Test::Commands>(10);
         channels.push(tx);
+        let tx_fin = tx_fin.clone();
         thread::spawn(move || {
             let mut machine = ma;
+            let mut fc = 0;
             loop{
                 match rx.recv() {
                     Some(x) => {
                         machine.execute(&x).unwrap();
+                        fc += 1;
+                        if fc == 10 {
+                            tx_fin.send(0);
+                        }
                     },
                     _ => ()
                 }
             }
         });
+    }
+    for ma in othertest_machines {
+        let (tx, rx) = chan::sync::<OtherTest::Commands>(10);
+        other_channels.push(tx);
+        let tx_fin = tx_fin.clone();
+        thread::spawn(move || {
+            let mut machine = ma;
+            let mut fc = 0;
+            loop{
+                match rx.recv() {
+                    Some(x) => {
+                        machine.execute(&x).unwrap();
+                        fc += 1;
+                        if fc == 10 {
+                            tx_fin.send(0);
+                        }
+                    },
+                    _ => ()
+                }
+            }
+        });
+    }
+    
+    {
+        let _a = tx_fin;
     }
 
     thread::spawn(move || {
@@ -59,8 +109,12 @@ fn main() {
                 Some(x) => {
                     match x {
                         Message::Test {id, cmd} => {
-                            println!("send to {}", id);
+                            //println!("send to {}", id);
                             channels[id].send(cmd);
+                        }
+                        Message::OtherTest {id, cmd} => {
+                            //println!("send to {}", id);
+                            other_channels[id].send(cmd);
                         }
                     }
                 },
@@ -69,13 +123,13 @@ fn main() {
         }
     });
 
-    tx.send(Message::Test {id:0, cmd: Test::Commands::Cmd1});
-    tx.send(Message::Test {id:0, cmd: Test::Commands::Cmd1});
-    tx.send(Message::Test {id:0, cmd: Test::Commands::Cmd1});
-    tx.send(Message::Test {id:0, cmd: Test::Commands::Cmd1});
-    tx.send(Message::Test {id:1, cmd: Test::Commands::Cmd1});
-    tx.send(Message::Test {id:1, cmd: Test::Commands::Cmd2});
-    tx.send(Message::Test {id:1, cmd: Test::Commands::Cmd1});
-
-    thread::sleep(std::time::Duration::from_secs(5));
+    for _ in 0..10 {
+        //println!("send");
+        tx.send(Message::Test { id: 0, cmd: Test::Commands::Cmd1 });
+        tx.send(Message::Test { id: 1, cmd: Test::Commands::Cmd1 });
+        //println!("send done");
+    }
+    //println!("wait all done");
+    rx_fin.recv();
+    rx_fin.recv();
 }
